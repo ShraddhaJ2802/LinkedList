ee05b473b0fd0b4eaea9915dbeac02525d0d1e99
Shraddha <jadhavshradddha5@gmail.com>
2023-02-21 11:24:54 +0530

    Deleting at middle

--- src/com/bridgelab/linkedlist/SinglyLinkedList.java
+++ src/com/bridgelab/linkedlist/SinglyLinkedList.java
@@ -110,6 +110,51 @@ public class SinglyLinkedList {
         System.out.println();
     }
 
+
+    //deleteFromMid() will delete a node from the middle of the list
+    void deleteFromMid() {
+        Node temp, current;
+//Checks if the list is empty
+        if(head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        else {
+
+            int count = (size % 2 == 0) ? (size/2) : ((size+1)/2);
+//Checks whether the head is equal to the tail or not, if yes then the list has only one node.
+            if( head != tail ) {
+                //Initially, temp will point to head
+                temp = head;
+                current = null;
+//Current will point to node previous to temp
+                //If temp is pointing to node 2 then current will point to node 1.
+                for(int i = 0; i < count-1; i++){
+                    current = temp;
+                    temp = temp.next;
+                }
+                if(current != null) {
+                    //temp is the middle that needs to be removed.
+                    //So, current node will point to node next to temp by skipping temp.
+                    current.next = temp.next;
+//Delete temp
+                    temp = null;
+                }
+                //If current points to NULL then, head and tail will point to node next to temp.
+                else {
+                    head = tail = temp.next;
+//Delete temp
+                    temp = null;
+                }
+            }
+//If the list contains only one element
+            //then it will remove it and both head and tail will point to NULL
+            else {
+                head = tail = null;
+            }
+        }
+        size--;
+    }
     public void deleteFromEnd() {
 
         //Checks if the list is empty
@@ -196,6 +241,13 @@ public class SinglyLinkedList {
 
         //Search for node 2 in the list
         sList.searchNode(30);
+
+        while(sList.head != null) {
+            sList.deleteFromMid();
+            //Printing updated list
+            System.out.println("Updated List: ");
+            sList.display();
+        }
 
     }
 }
